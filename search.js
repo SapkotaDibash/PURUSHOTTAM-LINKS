4e0f7606c85de487b692e2523dddfc0499ed9314
Dibash Sapkota _ <92871130+SapkotaDibash@users.noreply.github.com>
2023-01-28 16:42:33 +0545

    Update search.js

--- search.js
+++ search.js
@@ -1,24 +1,30 @@
+let TRANSPAENT_DIV_STATUS = false;
 $('#SearchInput').keyup(function () {
     const val = $(this).val();
     if (val.length == 0) {
         $('.transparent_BG').remove();
+        TRANSPAENT_DIV_STATUS = false;
         return;
     }
-    $('.transparent_BG').remove();
-    $('body').append(transparentBG);
+    if (TRANSPAENT_DIV_STATUS == false) {
+        TRANSPAENT_DIV_STATUS = true;
+        $('body').append(transparentBG);
+    }
 
 
 
 
     const filteredArrayList = search(val);
 
-    $('.filteredItems_Summarry').append('Found Items For : ' + val);
+    $('.filteredItems_Summarry').empty().append('Found Items For : ' + val);
     $('.filteredItems_Summarry').append('<br>Fonund Link Counts : ' + filteredArrayList.length);
     $('.filteredItems_Summarry').append('<br><hr>');
 
+    
+    $('.filteredItems_List').empty();
     for (let i = 0; i < filteredArrayList.length; i++) {
-        const r=filteredArrayList[i];
-        const SN = ( i + 1).toString().padStart(2, 0);
+        const r = filteredArrayList[i];
+        const SN = (i + 1).toString().padStart(2, 0);
         let HTML_LINKS = `${SN}. <a href="${r['link_obj']}">${r['display_name']}</a><br>`;
         $('.filteredItems_List').append(HTML_LINKS);
 
